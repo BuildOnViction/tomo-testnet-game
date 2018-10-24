b899268f075dafa8b3ceb00085a2f2f4610f4700
Minh Khai Do <khaihkd@gmail.com>
2018-10-24 15:55:20 +0700

    add config tomo send to user

--- sendTomo.js
+++ sendTomo.js
@@ -25,7 +25,7 @@ async function sendTomo() {
         let tx = await web3.eth.sendTransaction({
             to: walletAddress,
             from: currentWallet,
-            value: web3.utils.toWei('0.1', "ether"),
+            value: web3.utils.toWei(String(config.get('TOMO_EACH_USER')), "ether"),
             // gas: 100000,
             gasPrice: 100000,
         })
